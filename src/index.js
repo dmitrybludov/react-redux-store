8ecb0174876c2359a495f0657b8a5da5f7f7f1a9
Dmitry <bludovdmitry@gmail.com>
2022-03-18 16:19:41 +0200

    product page development. praparing data for the product page

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {Provider} from 'react-redux'
 import {routerMiddleware, ConnectedRouter} from 'connected-react-router'
 
 import createRootReducer from 'reducers'
-import Layout from 'containers/layout'
+import routes from 'routes'
 
 const history = createBrowserHistory()
 const middleWares = [thunk, routerMiddleware(history)]
@@ -22,7 +22,7 @@ const store = createStore(
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Layout />
+      {routes}
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
